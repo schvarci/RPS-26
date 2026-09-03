a35f31dd5ca07ffff231aa943d94d4c23cd696a5
schvarci <cvetan.patrik@gmail.com>
2026-09-03 09:45:24 +0000

    1.Vaja

--- Vaje/Uvod.py
+++ Vaje/Uvod.py
@@ -1,1 +1,8 @@
-print("Hello 1.Ri")
+def hello():
+    oddelek=input("Kateri oddelek si? ")
+    if oddelek.lower() == "1.ri":
+        print(f"Hello <3")
+    else:
+        print(f"Hello {oddelek}")
+if __name__=="__main__":
+    hello()

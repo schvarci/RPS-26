c0e39972a906ade5b8c7368375513beb16b4294c
schvarci <cvetan.patrik@gmail.com>
2026-09-03 09:59:40 +0000

    2.Vaja

--- Vaje/Uvod.py
+++ Vaje/Uvod.py
@@ -4,5 +4,15 @@ def hello():
         print(f"Hello <3")
     else:
         print(f"Hello {oddelek}")
+
+
+def postevanka():
+    x = int(input("Izberi število: "))
+#While ponavlajl dokler while___: ni resničen
+    št = 1
+    while št<=10:
+        print(f"{št}*{x}={št*x}")
+        št += 1     
+
 if __name__=="__main__":
-    hello()
+    postevanka()
